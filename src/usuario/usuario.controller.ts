4bbb9f8f622adc40d73b2ab2f776a03c5cb481bf
Gerson Migueis <gersonmigueis10@gmail.com>
2021-07-27 00:00:14 -0400

    [ADD] - Declarando no controlador

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
+import { Usuario } from '../usuario/usuario.entity';
 
 @Controller('users')
 export class UsuarioController {
   constructor(private usuarioService: UsuarioService) {}
   @Post()
-  public cria(@Body() usuario) {
+  public cria(@Body() usuario: Usuario) {
     const usuarioCriado = this.usuarioService.cria(usuario);
     return usuarioCriado;
   }
