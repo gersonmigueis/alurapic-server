4d53e8c1bbde9141b6221e494a100a2aa954bb04
Gerson Migueis <gersonmigueis10@gmail.com>
2021-07-27 23:59:21 -0400

    [Add] - Metodo de buscar pelo nome

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,12 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { Usuario } from './usuario.entity';
+import { Get } from '@nestjs/common';
 
 @Controller('users')
 export class UsuarioController {
   constructor(private usuarioService: UsuarioService) {}
+
+  @Get(':nomeUsuario')
+  public buscarPorNome(@Param('nomeUsuario') nomeUsuario: string) {
+    const usuarioEncontrado = this.usuarioService.buscarPorNome(nomeUsuario);
+
+    return usuarioEncontrado;
+  }
+
   @Post()
-  public cria(@Body() usuario: Usuario) {
+  public cria(@Body() usuario: Usuario): Usuario {
     const usuarioCriado = this.usuarioService.cria(usuario);
     return usuarioCriado;
   }
